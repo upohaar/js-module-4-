1a067d6f7744e086f3f42c7d98dbeaa0577a601b
Upohaar <sumaiyanobeupohaar@gmail.com>
2025-12-27 02:47:28 +0600

    find biggest number function create

--- function.js
+++ function.js
@@ -1,5 +1,26 @@
 function addition(a,b){
     const result=a+b;
-    console.log("result is ",result)
+    return result;
 }
-addition()
+const res = addition(2,8);
+//console.log(res)
+function giveName(){
+    const x = "upo"
+    return x;
+}
+const myName=giveName()
+//console.log(myName)
+
+function giveNumber(array){
+    let biggest =array[0]
+    for (i =1; i<array.length; i++){
+        if(array[i]>biggest){
+            biggest=array[i];
+            
+        }
+    } 
+    return biggest;
+}
+const arr= [34,45,67,3,556,7]
+result=giveNumber(arr)
+console.log(result)
--- loop.js
+++ loop.js
@@ -2,7 +2,7 @@
 //     // console.log("upo",i)
 // }
 const flowers=["jui","sapla","joba","golap"];
-console.log(flowers.length)
+//console.log(flowers.length)
 for (let i =0; i<flowers.length; i++){
     //console.log(flowers[i])
   
@@ -17,4 +17,24 @@ while(i>=0){
 for (let item of flowers){
  //   console.log(item)
 }
+let n =4;
 
+for(let i=1; i<=n; i++){
+    let row="";
+    for(let j=1; j<=n-i; j++){
+        row= row+ " ";}
+    for(let k=1; k<=2*i-1; k++){
+            row+="*";
+        }
+   // console.log(row)
+}
+
+
+numbers=[102,2,34,56,23,65]
+let biggest = numbers[0];
+for (let i=1 ; i<numbers.length; i++){
+    if(numbers[i]>biggest){
+        biggest=numbers[i]
+    }
+}
+console.log(biggest)
