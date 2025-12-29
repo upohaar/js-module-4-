43bd420f766fc9f655d2a5d1f6b6364130a44950
Upohaar <sumaiyanobeupohaar@gmail.com>
2025-12-29 21:31:22 +0600

    edit object

--- object.js
+++ object.js
@@ -8,11 +8,6 @@ const values = Object.values(person)
 console.log(keys)
 console.log(values)
 
-const person ={
-    name: "upo",
-    address: "dhamrai",
-    age:23
-}
 function info(per){
     result=per.name + per.address+"@gmail.com" ;
     return result;
